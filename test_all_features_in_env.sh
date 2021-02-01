5f824bb63519cfad2bc4c4afef75e6f6649ef98b
dgasull <dgasull@bsc.es>
2021-02-01 12:48:47 +0100

    Grouping tests to avoid timeouts

--- test_all_features_in_env.sh
+++ test_all_features_in_env.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-if [ "$#" -ne 3 ]; then
-    echo "ERROR: missing parameter. Usage $0 ENVIRONMENT ARCH IMAGE"
+if [ "$#" -ne 4 ]; then
+    echo "ERROR: missing parameter. Usage $0 TEST_PATTERN ENVIRONMENT ARCH IMAGE"
     exit 1
 fi
+TEST_PATTERN=$1
 TESTS=()
 pushd ./features/
-for f in *.feature; do
+for f in $TEST_PATTERN.feature; do
     FEATURE="${f%.feature}"
     printf '%s\n' "${FEATURE}"
     TESTS+=(${FEATURE})
 done
 popd
 mkdir -p ./allure-results
-ENVIRONMENT=$1
-ARCH=$2
-IMAGE=$3
+ENVIRONMENT=$2
+ARCH=$3
+IMAGE=$4
 EXIT_CODE=0
+echo "Going to test: ${TESTS[@]} "
 for TEST in ${TESTS[@]}; do
     ./test_feature.sh $TEST $ENVIRONMENT $ARCH $IMAGE
     RESULT=$?
