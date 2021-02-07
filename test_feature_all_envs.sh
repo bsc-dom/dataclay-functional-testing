021b91955c517bf7eae0928f541935e26ab85e71
dgasull <dgasull@bsc.es>
2021-02-07 10:42:40 +0100

    Fixed dynamicity docker-compose + misc in tests scripts

--- test_feature_all_envs.sh
+++ test_feature_all_envs.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "ERROR: missing parameter. Usage $0 TESTNAME"
+if [ "$#" -lt 1 ]; then
+    echo "ERROR: missing parameter. Usage $0 dataclay-packaging-path"
     exit 1
 fi
+status_code=$(curl -I -k -s http://localhost:5000/ | head -n 1 | cut -d ' ' -f 2)
+if [[ "$status_code" != "200" ]]; then
+    echo "ERROR: missing local docker registry. Start it with "
+    echo "    docker run -d -p 5000:5000 --restart=always --name registry registry:2 "
+    exit 1
+fi
+DATACLAY_PACKAGING=$1
 source ./BUILD_MATRIX.txt
 mkdir -p ./allure-results
 TEST=$1
@@ -10,6 +17,7 @@ EXIT_CODE=0
 for ENVIRONMENT in ${ENVIRONMENTS[@]}; do
     for ARCH in ${ARCHS[@]}; do
       for IMAGE in ${IMAGES[@]}; do
+          ./build.sh $DATACLAY_PACKAGING --images $IMAGE --arch $ARCH --environments $ENVIRONMENT
           ./test_feature.sh $TEST $ENVIRONMENT $ARCH $IMAGE
           RESULT=$?
           if [ $RESULT -ne 0 ]; then
